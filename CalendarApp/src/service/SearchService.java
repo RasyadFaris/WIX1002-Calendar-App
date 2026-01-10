490f590662b536709541b48c6e0fcbf491dd09ca
azhrn <abdullahzahran5@gmail.com>
2026-01-10 20:53:41 +0800

    add search service and statsservice from ghailan

---
+++ CalendarApp/src/service/SearchService.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SearchService {
+    public static List<Event> searchByTitle(List<Event> events, String query) {
+        return events.stream()
+                .filter(event -> event.getTitle().toLowerCase().contains(query.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+}
