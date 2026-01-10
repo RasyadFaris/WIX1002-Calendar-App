58c829be44b74e9287f3a95eb488dda15722629d
azhrn <abdullahzahran5@gmail.com>
2026-01-10 15:13:40 +0800

    Main

--- CalendarApp/src/Main.java
+++ CalendarApp/src/Main.java
@@ -1,4 +1,12 @@
+import view.MainFrame;
+import javax.swing.SwingUtilities;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        // Run GUI in the Event Dispatch Thread (Best Practice)
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
 }
