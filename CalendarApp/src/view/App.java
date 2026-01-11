f25a9ff019655d8b42c4b4d818a257c432e68f01
azhrn <abdullahzahran5@gmail.com>
2026-01-11 23:00:33 +0800

    remove unused import java.streamcollector

--- CalendarApp/src/view/App.java
+++ CalendarApp/src/view/App.java
@@ -34,7 +34,6 @@ import java.time.LocalTime;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class App extends Application {
 
