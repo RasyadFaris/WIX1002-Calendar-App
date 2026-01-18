998fa08f434dfb207d5c85c73108331403e9796e
Lutfi <raziqlutfy@gmail.com>
2026-01-19 01:08:00 +0800

    fix

--- CalendarApp/src/main/java/view/App.java
+++ CalendarApp/src/main/java/view/App.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
